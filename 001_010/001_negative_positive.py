e146ef82b842f63f2aafb03d9cbdd44e04a75850
Mina Luong <luongminhngoc29@gmail.com>
2025-01-10 17:09:40 +0700

    check negative positive number

---
+++ 001_010/001_negative_positive.py
@@ -0,0 +1,12 @@
+def check_negative_positive(n):
+    if n < 0:
+        print(n, "is negative")
+    elif n == 0:
+        print("is zero")
+    else:
+        print(n, "is positive")
+
+
+check_negative_positive(-4)
+check_negative_positive(10)
+check_negative_positive("anh")
