d3be2f42225bda84d7fd48b8d3b91d41300bd1d4
Mina Luong <luongminhngoc29@gmail.com>
2025-02-06 23:32:11 +0700

    add test for calculate_sum

--- 011_020/012_calculate_sum.py
+++ 011_020/012_calculate_sum.py
@@ -1,6 +1,21 @@
+import unittest
+
+
 def calculate_sum(n):
     result = (n * (n + 1)) / 2
     return result
 
-n = 9
-print("sum of from 1 to", n, "is", calculate_sum(n))
+
+class MyTestCase(unittest.TestCase):
+    def test_calculate_sum(self):
+        tests = [
+            # (n, 1+2+...+n)
+            (0, 0),
+            (1, 1),
+            (2, 3),
+            (3, 6),
+            (9, 45)
+        ]
+        for n, expected_sum in tests:
+            actual_sum = calculate_sum(n)
+            self.assertEqual(expected_sum, actual_sum, f"sum(1..{n}) should be {expected_sum} but got {actual_sum}")
