c0bf0c57dfa088e8f8bb5671de55c3781552c787
Mina Luong <luongminhngoc29@gmail.com>
2025-02-14 17:02:43 +0700

    count_elements_in_list

--- 021_030/030_count_element_in_list.py
+++ 021_030/030_count_element_in_list.py
@@ -5,4 +5,15 @@ print("9 appears", element_to_count, "times in list")
 element_to_count = list.count(1)
 print("1 appears", element_to_count, "times in list")
 
+list = [10, 20, 6, 4 * 5, 27, 4, 20]
+list.sort()
+print("my list after sort:", list)
 
+element_to_count = list.count(20)
+print("20 appears", element_to_count, "times in list")
+list.remove(20)
+list.remove(20)
+list.remove(20)
+print(list)
+sum = sum(list)
+print("sum of my list:", sum)
