e42e596c94b80390a986385db5a9eaf858e4f751
Mina Luong <luongminhngoc29@gmail.com>
2025-02-14 15:08:47 +0700

    sort_list

---
+++ 021_030/025_sort_list.py
@@ -0,0 +1,15 @@
+list = [10, 6, 9, 100, 4, 57]
+list.sort()
+print("my list:", list)
+
+numbers = [10, 6, 9, 100, 4, 57, 100]
+numbers.sort()
+print("my list:", numbers)
+
+numbers = [10, 6, 9, 100, 4, 57, 10 * 8]
+numbers.sort()
+print("my list:", numbers)
+
+numbers = [10, 6.8, 9, 80.1, 4, 57, 10 * 8]
+numbers.sort()
+print("my list:", numbers)
