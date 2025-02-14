deb5d208a8baa570cd5e302fb34d3abe954766bd
Mina Luong <luongminhngoc29@gmail.com>
2025-02-14 12:56:17 +0700

    remove_element

---
+++ 021_030/024_remove_element.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 3, 4, 5, 10, 14, 16, 1]
+print("original:", numbers)
+
+numbers.remove(10)
+print("after remove(10):", numbers)
+
+numbers.remove(14)
+print("after remove(14):", numbers)
+
+numbers.remove(1)
+print("after remove(1):", numbers)
+
+numbers.remove(15)
+print("after remove(15):", numbers)
