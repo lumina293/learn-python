cbf778d892a875319fd11b8784b82240c07e0793
Mina Luong <luongminhngoc29@gmail.com>
2025-02-14 16:50:08 +0700

    count_elements_in_list

---
+++ 021_030/030_count_element_in_list.py
@@ -0,0 +1,8 @@
+list = [10, 6, 9, 3 * 3, 70]
+element_to_count = list.count(9)
+print("9 appears", element_to_count, "times in list")
+
+element_to_count = list.count(1)
+print("1 appears", element_to_count, "times in list")
+
+
