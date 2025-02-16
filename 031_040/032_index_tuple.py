35db91117ed6cf69f074f8f0cbf8838715060797
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 11:38:19 +0700

    index_in_tuple

---
+++ 031_040/032_index_tuple.py
@@ -0,0 +1,11 @@
+tuple = (10, 9, 5, 7)
+n = tuple[2]
+print("The second element in the tuple is:", n)
+
+m = tuple[6]
+print("The sixth element in the tuple is:", m)
+
+a = tuple[3]
+print("The third element in the tuple is:", a)
+
+print("The second and sixth element in the tuple is:", n, a)
