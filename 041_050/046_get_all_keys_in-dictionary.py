ec3e6c3723a2b35997b28fbe050f4d6b6c123c13
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 22:40:14 +0700

    get_all_keys

---
+++ 041_050/046_get_all_keys_in-dictionary.py
@@ -0,0 +1,11 @@
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York'
+}
+print("Original Dictionary: ", my_dict)
+
+# get all keys in dictionary
+keys = my_dict.keys()
+
+print("Dictionary after get all keys: ", list(keys))
