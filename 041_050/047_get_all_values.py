eaf741d5ec6e63ae5c96e1f7d039c96979be000f
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 23:12:30 +0700

    get_all_values

---
+++ 041_050/047_get_all_values.py
@@ -0,0 +1,12 @@
+my_dict = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'New York'
+}
+values = my_dict.values()
+
+values_array = list(values)
+
+print("Array all of values in dictionary: ", values_array)
+
+print("Dictionary after get all of values: ", values)
