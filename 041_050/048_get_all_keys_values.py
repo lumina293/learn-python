2d5e6bc29ea25d2932af2730454052b2c6a07063
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 23:19:22 +0700

    get_all_key_value

---
+++ 041_050/048_get_all_keys_values.py
@@ -0,0 +1,14 @@
+dict3 = {
+    'color': 'red',
+    'shape': 'rectangle',
+}
+
+print("Original dictionary: ", dict3)
+
+# get all pairs of key-value
+items = dict3.items()
+
+# items to list
+items_list = list(items)
+
+print("The list of pairs of key-value in dictionary: ", items_list)
