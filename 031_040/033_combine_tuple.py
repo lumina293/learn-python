47d061186073763e504b741bb8179da5d084e408
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 11:45:19 +0700

    combine_tuple

---
+++ 031_040/033_combine_tuple.py
@@ -0,0 +1,12 @@
+tuple1 = (1, 3, 5, 7)
+tuple2 = (2, 4, 6, 8)
+combined_tuple = tuple1 + tuple2
+print("The new tuple after combining:", combined_tuple)
+
+tuple3 = (10, 9)
+combined_tuple = combined_tuple + tuple3
+print("The new tuple:", combined_tuple)
+
+tuple4 = (0, 100)
+combined_tuple = tuple1 + tuple2 + tuple4
+print("The third tuple:", combined_tuple)
