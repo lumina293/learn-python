2f61df5e69967c5d73457efbb7b4e2a80ccb2532
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 18:06:29 +0700

    get_value

--- 041_050/042_get_value_of_key.py
+++ 041_050/042_get_value_of_key.py
@@ -1,17 +1,20 @@
 # create a dictionary
-dict = {
+my_dict = {
     'name': 'Alace',
     'age': 24,
     'city': "New York"
 }
-print("Dictionary da tao:", dict)
+print("Dictionary da tao:", my_dict)
 
-# Enter key from user
+# # Enter key from user
 key = input("nhap tu khoa can truy cap: ")
+#
+# # Check key in dictionary
+# if key in my_dict:
+#     value = my_dict[key]
+#     print(f"gia tri cua '{key}' la: {value}")
+# else:
+#     print(f"'{key}' khong ton  tai trong dictionary")
 
-# Check key in dictionary
-if key in dict:
-    value = dict[key]
-    print(f"gia tri cua '{key}' la: {value}")
-else:
-    print(f"'{key}' khong ton  tai trong dictionary")
+value = my_dict.get(key)
+print(f"gia tri cua {key} la: {value} ")
