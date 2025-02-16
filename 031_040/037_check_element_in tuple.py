e276412ebe9139708719913a19903054e5818d4b
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 13:36:05 +0700

    check_element_in-tuple_using_in

---
+++ 031_040/037_check_element_in tuple.py
@@ -0,0 +1,6 @@
+t = (4, 7, 9, 3)
+element = int(input("nhap phan tu can kiem tra: "))
+if element in t:
+    print(f"{element} ton tai trong tuple")
+else:
+    print(f"{element} khong ton tai trong tuple")
