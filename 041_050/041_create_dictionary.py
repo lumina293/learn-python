c02920e8cb79a163786e0c27f6933b2e9b28f151
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 17:23:43 +0700

    create_a_dictionary

---
+++ 041_050/041_create_dictionary.py
@@ -0,0 +1,19 @@
+dict = {}
+dict['name'] = 'Alace'
+dict['age'] = 25
+dict['city'] = 'New York'
+print("Dictionary da tao:", dict)
+
+dict_1 = {
+    'name': 'Lisa',
+    'age': 34,
+    'city': 'Singapore'
+}
+print("Dictionary da tao:", dict_1)
+
+dict_2 = {
+    'name': 'Bod',
+    'age': 30,
+    'city': 'Los Angeles'
+}
+print("Dictionary da tao:", dict_2)
