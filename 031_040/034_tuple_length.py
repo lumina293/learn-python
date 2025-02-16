b6690f76127cee4db5a95300e50d0daca67cd334
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 12:30:43 +0700

    tuple_length

---
+++ 031_040/034_tuple_length.py
@@ -0,0 +1,11 @@
+tuple = (20, 30, 40, 50, 60, 70)
+tuple_length = len(tuple)
+print("The length of tuple is:", tuple_length)
+
+tuple = (10, 20, 30, 40, 50, 60, 70, 80, 10, 20)
+tuple_length = len(tuple)
+print(tuple_length)
+
+tuple = (10, 3, 5 * 6, 9 / 3)
+tuple_length = len(tuple)
+print(tuple_length)
