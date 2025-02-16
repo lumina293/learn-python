70e289df45b2d75eb1ccfb22a0aaa1c3a2b0acc7
Mina Luong <luongminhngoc29@gmail.com>
2025-02-16 13:46:32 +0700

    find_max_element

---
+++ 031_040/039_find_max_element.py
@@ -0,0 +1,4 @@
+tuple = (100, 400, 40 * 8, 400)
+max_element = max(tuple)
+count = tuple.count(max_element)
+print("The max element in tuple is:",max_element, f"and {max_element} appears {count} times in tuple")
