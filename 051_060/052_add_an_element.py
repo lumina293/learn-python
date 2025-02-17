d84b9b70d0d8ad9b1e9b338505a6a44b71bf93a2
Mina Luong <luongminhngoc29@gmail.com>
2025-02-17 15:50:59 +0700

    add_elements

---
+++ 051_060/052_add_an_element.py
@@ -0,0 +1,11 @@
+set1 = {'color', 'shape'}
+print("Original set: ", set1)
+
+# add element
+# c1 using add()
+set1.add('flower')
+print("the new set: ", set1)
+
+# c2 using update([])
+set1.update(['blue', 1, "hoa"])
+print(set1)
