5512ff61d588dc050d44099c7c87030bf14a620b
Mina Luong <luongminhngoc29@gmail.com>
2025-02-18 23:13:12 +0700

    create_a_string

--- 061_070/061_create_a_string.py
+++ 061_070/061_create_a_string.py
@@ -6,3 +6,37 @@ print(str1)
 
 str1 = "Hi, JJ", "Love you"
 print(str1)
+
+str2 = """hello,
+world"""
+print(str2)
+
+num = 123
+str2 = str(num)
+print(str2)
+
+int1 = 34
+str3 = str(int1)
+print(str3)
+
+str4 = "hi, " + "Mina"
+print(str4)
+
+# str4 = str1 + str4
+# print(str4)
+
+name = "JJ"
+str6 = f"Hi, {name}"
+print(str6)
+
+name = "Lorem"
+str7 = "Hi, {}".format(name)
+print(str7)
+
+name = "Lorem and &, 1"
+str8 = "Hi, {}".format(name)
+print(str8)
+
+name = "JJ", "#"
+str6 = f"Hi, {name}"
+print(str6)
