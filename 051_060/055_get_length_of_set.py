139db646d9a76ce3452e470081af08c0bc508d16
Mina Luong <luongminhngoc29@gmail.com>
2025-02-18 22:22:14 +0700

    length_of_elements

---
+++ 051_060/055_get_length_of_set.py
@@ -0,0 +1,21 @@
+set1 = {'banana', 'cherry', 'apple'}
+print(set1)
+
+# length set len()
+length = len(set1)
+print("The length of set is: ", length)
+
+set2 = {'a', 'b', 'c', 'a'}
+print(set2)
+length = len(set2)
+print(length)
+
+set3 = {'s', 'f', '3', 4}
+print(set3)
+length = len(set3)
+print(length)
+
+set4 = {'s', 'f', '@', 4}
+print(set4)
+length = len(set4)
+print(length)
