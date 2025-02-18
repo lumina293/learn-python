3083a38d6b295a8f8511a70882e3f8412b54a6e2
Mina Luong <luongminhngoc29@gmail.com>
2025-02-18 22:39:49 +0700

    set_intersection

---
+++ 051_060/057_find_intersection_of_sets.py
@@ -0,0 +1,26 @@
+set1 = {'apple', 'banana', 'cherry'}
+set2 = {'banana', 'cherry', 'durian'}
+
+set_intersection = set1 & set2
+print("giao cua hai set la: ", set_intersection)
+
+set3 = {1, 3, 5}
+set_intersection = set1 & set3
+print(set_intersection)
+
+set1 = {'apple', 'watermelon'}
+set2 = {'banana', 'cherry', 'durian'}
+set_intersection = set1 & set2
+print(set_intersection)
+
+set1 = {'apple', 'banana', 'cherry'}
+set2 = {'banana', 'cherry', 'durian'}
+set4 = {'durian', 'apple', 'banana'}
+set_intersection = set1 & set2 & set4
+print(set_intersection)
+
+set1 = {'apple', 'banana', 'cherry'}
+set2 = {'banana', 'cherry', 'durian'}
+set4 = {'durian', 'apple'}
+set_intersection = set1 & set2 & set4
+print(set_intersection)
