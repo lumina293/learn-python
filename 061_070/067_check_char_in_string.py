21387b46ff4fd31c4d64d18f64c4fa7d09e9ebb0
Mina Luong <luongminhngoc29@gmail.com>
2025-02-20 11:32:50 +0700

    check_char

---
+++ 061_070/067_check_char_in_string.py
@@ -0,0 +1,9 @@
+# str1 = "Look at me JJ"
+# word = input("Nhap tu muon kiem tra: ")
+# contains_word = word in str1
+# print(contains_word)
+
+str2 = "bac 134"
+word = "1345"
+contains_word = str2.find(word) != -1
+print(contains_word)
