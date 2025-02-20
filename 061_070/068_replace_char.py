63dac9912338b1f62808fb82b6475b68bb69d7c4
Mina Luong <luongminhngoc29@gmail.com>
2025-02-20 11:36:49 +0700

    replace_char

---
+++ 061_070/068_replace_char.py
@@ -0,0 +1,11 @@
+str1 = "Hi, Brian"
+word_to_replace = "Brian"
+replacement_word = "Mina"
+new_str = str1.replace(word_to_replace, replacement_word)
+print(new_str)
+
+str2 = "Look at me, please"
+word_to_replace = input("Nhap tu muon thay: ")
+replacement_word = input("Nhap tu thay: ")
+new_str2 = str2.replace(word_to_replace, replacement_word)
+print(new_str2)
