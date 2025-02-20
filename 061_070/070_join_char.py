9911223698c05cdb589cbbb8dbdfaf2cbc39f07b
Mina Luong <luongminhngoc29@gmail.com>
2025-02-20 15:20:00 +0700

    join_char

---
+++ 061_070/070_join_char.py
@@ -0,0 +1,21 @@
+words_list = ["Hi", "Mina", "welcome", "to", "Python"]
+joined_str = ''.join(words_list)
+print(joined_str)
+
+
+words_list = ["Hi", "Mina", "welcome", "to", "Python"]
+joined_str = ' '.join(words_list)
+print(joined_str)
+
+joined_str = '.'.join(words_list)
+print(joined_str)
+
+joined_str = ' . '.join(words_list)
+print(joined_str)
+
+joined_str = ','.join(words_list)
+print(joined_str)
+
+joined_str = ';'.join(words_list)
+print(joined_str)
+
