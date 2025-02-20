b29ccf5b72ec1838d52801ffbc4aefe5edb3c34d
Mina Luong <luongminhngoc29@gmail.com>
2025-02-20 10:45:21 +0700

    count_char

---
+++ 061_070/066_count_char.py
@@ -0,0 +1,10 @@
+str1 = "Hi, I am Min a"
+lower_char = str1.lower()
+print(lower_char)
+char_to_count = "i"
+count = lower_char.count(char_to_count)
+print(f"So lan xuat hien cua '{char_to_count}' la: ", count)
+
+char_to_count = " "
+count = str1.count(char_to_count)
+print(f"So lan xuat hien cua '{char_to_count}' la: ", count)
